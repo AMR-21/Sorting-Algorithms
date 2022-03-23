f329f087624f6e6fbfb9854c98d12835a9b0f5cd
amr215 <amryasser52001@gmail.com>
2022-03-23 18:41:14 +0200

    Removing classes

--- Algorithms.py
+++ Algorithms.py
@@ -6,33 +6,30 @@
 
 # IDEA: why not add standard and our customized implementation and compare between them
 
-class QuickSort:
-    def quicksort(self, test_array):
+
+def quicksort(self, test_array):
         A = 3
 
 
-class MergeSort:
-    def mergesort(self, test_array):
+
+def mergesort(self, test_array):
         A = 3
 
 
-class SelectionSort:
-    def selectionsort(self, test_array):
+def selectionsort(self, test_array):
         A = 3
 
 
-class InsertionSort:
-    def insertion_sort(self,test_array):
-        for i in range(1, len(test_array)):
-            key = test_array[i]
-            j = i-1
-            while j >= 0 and key < test_array[j]:
-                test_array[j + 1] = test_array[j]
-                j -= 1
-            test_array[j + 1] = key
+def insertion_sort(self,test_array):
+     for i in range(1, len(test_array)):
+        key = test_array[i]
+        j = i-1
+        while j >= 0 and key < test_array[j]:
+            test_array[j + 1] = test_array[j]
+            j -= 1
+        test_array[j + 1] = key
 
 
 
-class HybridSort:
-    def hybridsort(self, test_array):
-        A = 3
+def hybridsort(self, test_array):
+    A = 3
